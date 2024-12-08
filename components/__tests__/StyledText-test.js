991fe5f48b82b2224f064e7a4cd374f7a400447f
debasish <debasish@weassemble.team>
2024-12-08 23:22:49 +0530

    order change

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -1,10 +1,12 @@
-import * as React from 'react';
-import renderer from 'react-test-renderer';
+import * as React from "react";
+import renderer from "react-test-renderer";
 
-import { MonoText } from '../StyledText';
-
-it(`renders correctly`, () => {
-  const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
-
-  expect(tree).toMatchSnapshot();
+describe("Some test set", () => {
+  // We are marking the test function call as async
+  test("Name of the test", async () => {
+    // import { MonoText } from '../StyledText';
+    const { MonoText } = require("../StyledText");
+    const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
